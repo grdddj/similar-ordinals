dd698fdb33aec68062eb2bb4de9157cfc88fb352
grdddj <jiri.musil06@seznam.cz>
2023-04-11 22:27:18 +0200

    send more information from the API

--- api.py
+++ api.py
@@ -1,26 +1,38 @@
 # Deployed by:
-# uvicorn api:app --reload --host 0.0.0.0 --port 8000
+# uvicorn api:app --reload --host 0.0.0.0 --port 8001
 from __future__ import annotations
 
 from fastapi import FastAPI, Query, UploadFile
 
-from common import bytes_to_hash, db_file
+from common import Match, bytes_to_hash, db_file
+from db_data import InscriptionModel
 from get_matches_rust import get_matches
 
 app = FastAPI()
 
 
-# curl http://localhost:8000/ord_id/123?top_n=10
+def matches_to_api_result(matches: list[Match]) -> list[dict]:
+    result: list[dict] = []
+    for match in matches:
+        match_ord_id = match["ord_id"]
+        inscription = InscriptionModel.by_id(int(match_ord_id))
+        result.append(inscription.dict())
+    return result
+
+
+# curl http://localhost:8001/ord_id/123?top_n=10
 @app.get("/ord_id/{ord_id}")
 async def by_ord_id(ord_id: int, top_n: int = Query(20)):
     matches = get_matches(db_file, str(ord_id), "", top_n)
-    return {"matches": matches}
+    result = matches_to_api_result(matches)
+    return {"result": result}
 
 
-# curl -X POST -H "Content-Type: multipart/form-data" -F "file=@images/1.jpg" http://localhost:8000/file?top_n=10
+# curl -X POST -H "Content-Type: multipart/form-data" -F "file=@images/1.jpg" http://localhost:8001/file?top_n=10
 @app.post("/file")
 async def by_custom_file(file: UploadFile, top_n: int = Query(20)):
     file_bytes = await file.read()
     file_hash = bytes_to_hash(file_bytes)
     matches = get_matches(db_file, "", file_hash, top_n)
-    return {"matches": matches}
+    result = matches_to_api_result(matches)
+    return {"result": result}
